64367b723c3a9f60aab8c7f9798cedc30a6bcc2e
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-06-17 19:10:44 +0530

    Finally completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./Components/Background/Background";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
   let heroData = [
@@ -10,10 +11,24 @@ const App = () => {
   ];
   const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(()=>{
+    setInterval(() => {
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 3000);
+  },[])
+
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='nav'>
+    <div className="nav">
       <div className="nav-logo">Ev-olution</div>
       <ul className="nav-menu">
         <li>Home</li>
         <li>Explore</li>
         <li>About</li>
-        <li className='nav-contact'>Contact</li>
+        <li className="nav-contact">Contact</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
