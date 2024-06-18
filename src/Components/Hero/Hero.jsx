6e1cb2330be0b2af32acc98ec6fb7749260d19ce
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-06-18 11:06:16 +0530

    Some Changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import "./Hero.css";
+import './Hero.css';
 import arrow_btn from "../../assets/arrow_btn.png";
 import play_icon from "../../assets/play_icon.png";
 import pause_icon from "../../assets/pause_icon.png";
